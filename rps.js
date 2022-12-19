661056ceeece8e8dd13fd66a3189c7a454de0e8c
Yorkzap <118312686+yorkzap@users.noreply.github.com>
2022-12-19 01:41:31 -0800

    Add invalidchoice fallback as a hook for future

--- rps.js
+++ rps.js
@@ -1,7 +1,5 @@
 //Make rock paper scissors against computer
 
-//only allow "Rock" or "Paper" or "Scissors" as the prompt using allowedPlayerInput
-//play a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" using playRoundFunction
 //game function will use playRound function as a callback to play 5 total round of the game and keep the score. At the end, will display the winner. Choose a for loop for this one.
 //Create helper functions as needed
 
@@ -10,7 +8,7 @@
 let rock = "Rock";
 let paper = "Paper";
 let scissors = "Scissors";
-let invalidChoice;
+let invalidChoice = "Wrong input"
 
 
 // Return random rock/paper/scissor computer choice
@@ -26,10 +24,10 @@ function getComputerChoice () {
     }
 }
 
-//testing Computer choice
+//Testing Computer choice
 console.log("What computer picked (" + getComputerChoice()+ ")");
 
-//prompt a choice from the player and capitalize first letter, lower case the rest using casePlayerInput function
+//Prompt a choice from the player and capitalize first letter, lower case the rest using casePlayerInput function
 function casePlayerInput () {
     let playerInput = prompt("Input Rock or Paper or Scissors");
     playerInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
@@ -37,19 +35,27 @@ function casePlayerInput () {
 }
 
 
-// only allow rock/paper/scissors input
+// Allow only rock/paper/scissors input
 function allowedPlayerInput(playerInput) {
     switch (true) {
         case (playerInput == rock || playerInput == rock + "s"):
             console.log("What you picked (" + rock+ ")");
+            return rock;
 
         case (playerInput == paper || playerInput == paper + "s"):
             console.log("What you picked (" + paper+ ")");
+            return paper;
 
         case (playerInput == scissors || playerInput == scissors.slice(0,-1)):
             console.log("What you picked (" + scissors+ ")");
-
+            return scissors;
+        default:
+            console.log(invalidChoice + "Try again");
+            return invalidChoice;
     }
 }
 
 allowedPlayerInput(casePlayerInput());
+
+
+//play a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" using playRoundFunction
